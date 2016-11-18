a31d173fb15ac843c746bc904e33e7dc763e61fc
cefeangel <cefeangel@hotmail.com>
2016-11-18 14:41:49 -0200

    Primeiro Commint Bug

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
             echo 'Teste';
-            echo 'Teste Desenvolvimento';
+            echo 'Teste Desenvolvimento 10';
         ?>
     </body>
 </html>
